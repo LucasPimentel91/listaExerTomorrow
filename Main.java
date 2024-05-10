2a495554fb6228898a7fa353989c5a3a112a81e0
Lucas <placte40@gmail.com>
2024-05-10 09:36:33 -0500

    "Exercicio 3 terminado"

--- Main.java
+++ Main.java
@@ -9,6 +9,16 @@ public class Main {
         Triangulo retangulo = new Triangulo();
         System.out.println("Area: " + retangulo.area(2, 3));
         System.out.println("Area: " + retangulo.perimetro(2, 3));
-
+        System.out.println("\n");
+        Produto leite = new Produto();
+        Produto arroz = new Produto();
+        leite.cadastro("Leite", 10.9, 5);
+        arroz.cadastro("Arroz", 6, 10);
+        leite.aumentarStoq(10);
+        leite.diminuirStoq(15);
+        arroz.aumentarStoq(10);
+        arroz.diminuirStoq(5);
+        leite.sobreProduto();
+        arroz.sobreProduto();
     }
 }
--- Produto.java
+++ Produto.java
@@ -1,3 +1,23 @@
 public class Produto {
+    private String nome;
+    private double preco;
+    private int qtdEstoque;
+
+    public void cadastro(String texto, double valor, int quantidade){
+        this.nome = texto;
+        this.preco = valor;
+        this.qtdEstoque= quantidade;
+    }
+    public void diminuirStoq(int num){
+        qtdEstoque-=num;
+    }
+    public void aumentarStoq(int num){
+        qtdEstoque+=num;
+    }
+    public void sobreProduto(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+        System.out.println("Quantidade em estoque: " + qtdEstoque);
+    }
 
 }
