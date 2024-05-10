d97ded559e90a0f079597be459d593004cfb78f8
Lucas <placte40@gmail.com>
2024-05-10 08:20:05 -0500

    "exer 1 e 2"

---
+++ Triangulo.java
@@ -0,0 +1,11 @@
+public class Triangulo {
+    private double base;
+    private double altura;
+
+    public double area(double base, double altura){
+        return (base * altura) / 2;
+    }
+    public double perimetro(double base, double altura){
+
+    }
+}
